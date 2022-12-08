997e53701f53d08d6692fb83d4c4f2d4e2db3c55
Till Theis <theis.till@gmail.com>
2022-12-08 22:37:40 +0100

    day 1.1 fast

--- src/bin/day_01_1.rs
+++ src/bin/day_01_1.rs
@@ -1,72 +1,38 @@
 extern crate core;
 
 use std::fs::File;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Lines};
 
 fn main() {
-    println!(
-        "{}",
-        most_calories(&all_calorie_groups_from_file("src/bin/day_01_input.txt"))
-    );
+    println!("{}", go("src/bin/day_01_input.txt"));
 }
 
-fn all_calorie_groups_from_file(path: &str) -> Vec<Vec<i32>> {
+fn go(path: &str) -> i32 {
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
+    let mut lines: Lines<BufReader<File>> = reader.lines();
 
-    let mut all_calorie_groups: Vec<Vec<i32>> = vec![];
-    let mut calorie_group: Vec<i32> = vec![];
-    for line in reader.lines() {
-        let line = line.unwrap();
+    let mut max = 0;
+    let mut sum = 0;
+
+    while let Some(Ok(line)) = lines.next() {
         if line.is_empty() {
-            all_calorie_groups.push(calorie_group);
-            calorie_group = vec![];
+            max = max.max(sum);
+            sum = 0;
         } else {
-            calorie_group.push(line.parse::<i32>().unwrap())
+            sum += line.parse::<i32>().unwrap();
         }
     }
-    all_calorie_groups.push(calorie_group);
 
-    all_calorie_groups
-}
-
-fn most_calories(all_calorie_groups: &Vec<Vec<i32>>) -> i32 {
-    all_calorie_groups
-        .iter()
-        .map(|arr| arr.iter().sum())
-        .max()
-        .unwrap()
+    max
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{all_calorie_groups_from_file, most_calories};
+    use crate::go;
 
     #[test]
-    fn all_calorie_groups_from_file_works() {
-        assert_eq!(
-            all_calorie_groups_from_file("src/bin/day_01_test_input.txt"),
-            vec![
-                vec![1000, 2000, 3000],
-                vec![4000],
-                vec![5000, 6000],
-                vec![7000, 8000, 9000],
-                vec![10000]
-            ]
-        );
-    }
-
-    #[test]
-    fn most_calories_works() {
-        assert_eq!(
-            most_calories(&vec![
-                vec![1000, 2000, 3000],
-                vec![4000],
-                vec![5000, 6000],
-                vec![7000, 8000, 9000],
-                vec![10000]
-            ]),
-            24000
-        );
+    fn go_works() {
+        assert_eq!(go("src/bin/day_01_test_input.txt"), 24000);
     }
 }
